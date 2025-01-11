1e5d2a6bd76dd7c74b550de36730302ddfd12cf9
Rajib Chowdhury <mysuccessstairs@hotmail.com>
2025-01-11 15:40:57 +0600

    linux build test done

--- linux-build.sh
+++ linux-build.sh
@@ -8,22 +8,22 @@ mkdir build
 cd build
 
 
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DQUICKFIX_TESTS=OFF -DQUICKFIX_EXAMPLES=OFF -DCMAKE_CXX_FLAGS="-O1" -DHAVE_SSL=ON -DHAVE_POSTGRESQL=ON -DCMAKE_INSTALL_PREFIX="/home/fsys/fsys/dependency/quickfix/linux-x64" ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DQUICKFIX_SHARED_LIBS=OFF -DQUICKFIX_TESTS=OFF -DQUICKFIX_EXAMPLES=OFF -DCMAKE_CXX_FLAGS="-O1" -DHAVE_SSL=ON -DHAVE_POSTGRESQL=ON -DCMAKE_INSTALL_PREFIX="/usr/local/" ..
 make -j4
 make install
 
-# Update the LD_LIBRARY_PATH for Dynamic Libraries
-# If the QuickFIX library is a shared library (.so file), you'll need to ensure your system can find it by updating the LD_LIBRARY_PATH environment variable.
-export LD_LIBRARY_PATH=/home/fsys/fsys/dependency/quickfix/lib/linux-x64:$LD_LIBRARY_PATH
-source ~/.bashrc  # or `source ~/.zshrc` if you're using Zsh
-# nstall to System Directories (Optional)
-sudo cp /home/fsys/fsys/dependency/quickfix/linux-x64/lib/* /usr/local/lib/
-sudo cp -r /home/fsys/fsys/dependency/quickfix/linux-x64/include/* /usr/local/include/
 
-#Create the Symbolic Links for libquickfix.so
-sudo ln -sf /usr/local/lib/libquickfix.so.16 /usr/local/lib/libquickfix.so
+#link with quickfix
+# Set QuickFix root directory and find the OpenSSL package
 
-sudo ldconfig -p | grep quickfix
-# must be display this result with version
-# libquickfix.so.16 (libc6,x86-64) => /usr/local/lib/libquickfix.so.16
-# libquickfix.so (libc6,x86-64) => /usr/local/lib/libquickfix.so
+set(QUICKFIX_LIBRARIES /usr/local/lib/libquickfix.a)
+set(QUICKFIX_INCLUDE_DIR /usr/local/include/quickfix)
+
+if(QUICKFIX_LIBRARIES AND QUICKFIX_INCLUDE_DIR)
+    message(STATUS "QuickFix Include Directory: ${QUICKFIX_INCLUDE_DIR}")
+    message(STATUS "QuickFix Libraries: ${QUICKFIX_LIBRARIES}")
+    target_include_directories(${EXECUTABLE_NAME} PRIVATE ${QUICKFIX_INCLUDE_DIR})
+    target_link_libraries(${EXECUTABLE_NAME} PRIVATE ${QUICKFIX_LIBRARIES})
+else()
+    message(FATAL_ERROR "QuickFix not found!")
+endif()
